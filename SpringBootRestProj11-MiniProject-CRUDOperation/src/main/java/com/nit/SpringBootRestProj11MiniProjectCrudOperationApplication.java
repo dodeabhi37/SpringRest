3b09137ba1a20e94e2c56cccbf3e7dd057d6bf70
DELL <DELL@DESKTOP-6AABL21>
2023-03-20 11:29:48 +0530

    Application Creation

---
+++ SpringBootRestProj11-MiniProject-CRUDOperation/src/main/java/com/nit/SpringBootRestProj11MiniProjectCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestProj11MiniProjectCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestProj11MiniProjectCrudOperationApplication.class, args);
+	}
+
+}
